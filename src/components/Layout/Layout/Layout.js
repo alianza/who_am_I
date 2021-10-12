fa9a5cfc1b4d6db78708c171e3677cba66a6a5db
Alianza <janwillemvanbremen@live.nl>
2021-10-12 16:59:38 +0200

    Fixed responsive menu

--- src/components/Layout/Layout/Layout.js
+++ src/components/Layout/Layout/Layout.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Layout(props) {
     return (
-        <div id={'content'} className={'absolute w-full p-4 flex flex-col gap-8 mt-12'}>
+        <div id={'content'} className={'absolute w-full p-4 flex flex-col gap-8 mt-16'}>
             {props.children}
         </div>
     );
--- src/components/Layout/Menu/Menu.js
+++ src/components/Layout/Menu/Menu.js
@@ -3,10 +3,10 @@ import { NavLink} from "react-router-dom";
 
 function Menu() {
     return (
-    <nav id={"menu"} className={'bg-primary absolute right-0 flex justify-end h-12'}>
-        <ul className={'flex gap-x-4 mr-4 items-center'}>
-            <NavLink exact to={"/"} className={"p-2"}>Home</NavLink>
-            <NavLink to={"/about"} className={"p-2"}>About</NavLink>
+    <nav id={"menu"} className={'h-16 bg-primary absolute w-full flex justify-center sm:justify-end'}>
+        <ul className={'flex items-center'}>
+            <NavLink exact to={"/"} className={"p-4"}>Home</NavLink>
+            <NavLink to={"/about"} className={"p-4"}>About</NavLink>
         </ul>
     </nav>);
 }
